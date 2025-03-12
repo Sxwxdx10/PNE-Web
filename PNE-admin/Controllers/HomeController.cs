0a46497341d49035766af60770df5aae8d36e40a
lixk1901 <lixk1901@usherbrooke.ca>
2025-03-11 20:38:09 -0400

    Menu_Déroulante_Accès_et_Logout

--- PNE-admin/Controllers/HomeController.cs
+++ PNE-admin/Controllers/HomeController.cs
@@ -28,5 +28,12 @@ namespace PNE_admin.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Profil()
+        {
+            return View();
+        }
+
     }
+
 }
